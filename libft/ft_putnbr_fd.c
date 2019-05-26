23b33b9def65306a47661a91473639c740f7b193
Dagny Shirl <dshirl@mi-p5.21-school.ru>
2019-05-26 20:33:06 +0300

    ok

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -1,29 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putendl.c                                       :+:      :+:    :+:   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dshirl <dshirl@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/22 16:30:18 by dshirl            #+#    #+#             */
-/*   Updated: 2019/05/26 16:55:03 by dshirl           ###   ########.fr       */
+/*   Created: 2019/05/26 17:15:14 by dshirl            #+#    #+#             */
+/*   Updated: 2019/05/26 20:29:35 by dshirl           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putendl(char const *s)
+void	ft_putnbr_fd(int n, int fd)
 {
-	unsigned int	i;
-
-	i = 0;
-	if (s)
-	{
-		while (s[i] != '\0')
-		{
-			write(1, &s[i], 1);
-			i++;
-		}
-		write(1, "\n", 1);
-	}
+	ft_putstr_fd(ft_itoa(n), fd);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: dshirl <dshirl@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/22 19:32:36 by dshirl            #+#    #+#             */
-/*   Updated: 2019/05/22 19:24:26 by dshirl           ###   ########.fr       */
+/*   Updated: 2019/05/26 17:42:41 by dshirl           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,17 +61,17 @@ int ft_strnequ(char const *s1, char const *s2, size_t n);
 char *ft_strsub(char const *s, unsigned int start, size_t len);
 char *ft_strjoin(char const *s1, char const *s2);
 char *ft_strtrim(char const *s);
-/* char **ft_strsplit(char const *s, char c);
-char *ft_itoa(int n); */
+/* char **ft_strsplit(char const *s, char c); */
+char *ft_itoa(int n);
 void ft_putchar(char c);
 void ft_putstr(char const *s);
 void ft_putendl(char const *s);
 void ft_putnbr(int n);
 void ft_putchar_fd(char c, int fd);
-/* void ft_putstr_fd(char const *s, int fd);
+void ft_putstr_fd(char const *s, int fd);
 void ft_putendl_fd(char const *s, int fd);
 void ft_putnbr_fd(int n, int fd);
-t_list *ft_lstnew(void const *content, size_t content_size);
+/* t_list *ft_lstnew(void const *content, size_t content_size);
 void ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void ft_lstadd(t_list **alst, t_list *new);
